4d0f87f8eeca721a03524ee0c12c30c18e6daf6d
itokeso <61177741+itokeso@users.noreply.github.com>
2020-05-02 00:44:22 +0900

    blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index 
-    @posts = Post.all
+    @posts = Post.includes(:user)
   end 
 
   def new
@@ -9,8 +9,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-    if @post.save 
+    post = Post.create(post_params)
+    if post.save 
       redirect_to root_path
     else 
       render :new
@@ -21,9 +21,32 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      redirect_to post_path(post.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    if post.destroy
+      redirect_to root_path
+      
+    else
+      render :show
+    end
+  end
+
   private
 
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content).merge(user_id: current_user.id)
   end
 end
